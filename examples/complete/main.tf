8d5f1caa59506a498c9ad3fd968bd9e1be63d323
shanye <shanye.xzq@alibaba-inc.com>
2026-04-22 18:17:48 +0800

    feat!: add provider version constraint for aliyun/alicloud

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -19,7 +19,7 @@ data "alicloud_images" "default" {
 
 module "vpc" {
   source  = "alibaba/vpc/alicloud"
-  version = "~>1.11"
+  version = "2.0.0"
 
   create             = true
   vpc_cidr           = "172.16.0.0/16"
@@ -32,7 +32,7 @@ module "vpc" {
 ##################################################################
 module "security_group" {
   source  = "alibaba/security-group/alicloud"
-  version = "2.4.1"
+  version = "3.0.0"
 
 
   vpc_id = module.vpc.this_vpc_id
